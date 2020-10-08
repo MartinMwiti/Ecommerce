df962a179da196d8ca9a034b747f3a155b3c828b
MartinMwiti <martinmwiti007@gmail.com>
2020-10-08 19:32:13 +0300

    update

--- online_store/lib/screens/sign_in/components/body.dart
+++ online_store/lib/screens/sign_in/components/body.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:online_store/components/custom_suffix_icon.dart';
-import 'package:online_store/components/default_button.dart';
-import 'package:online_store/components/form_error.dart';
-import 'package:online_store/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_store/screens/sign_in/components/sign_form.dart';
 import 'package:online_store/size_config.dart';
 
 class Body extends StatelessWidget {
@@ -29,7 +27,24 @@ class Body extends StatelessWidget {
                 "Sign In with your email and password \n or continue with social media.",
                 textAlign: TextAlign.center,
               ),
-              SignForm()
+              SignForm(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SocialCard(
+                    icon: "assets/icons/facebook-2.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/google-icon.svg",
+                    press: () {},
+                  ),
+                  SocialCard(
+                    icon: "assets/icons/facebook-2.svg",
+                    press: () {},
+                  )
+                ],
+              )
             ],
           ),
         ),
@@ -38,137 +53,29 @@ class Body extends StatelessWidget {
   }
 }
 
-class SignForm extends StatefulWidget {
-  @override
-  _SignFormState createState() => _SignFormState();
-}
+class SocialCard extends StatelessWidget {
+  const SocialCard({
+    Key key,
+    this.icon,
+    this.press,
+  }) : super(key: key);
 
-class _SignFormState extends State<SignForm> {
-  // GlobalKey This uniquely identifies the Form , and allows validation of the form in a later step.
-  final _formKey = GlobalKey<FormState>();
-  String email;
-  String password;
-  final List<String> errors = [];
+  final String icon;
+  final Function press;
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          // TextFormField - Creates a [FormField] that contains a [TextField].
-          buildEmailFormField(),
-          SizedBox(height: getProportionateScreenHeight(20)),
-          buildPasswordFormField(),
-          SizedBox(height: getProportionateScreenHeight(20)),
-          FormError(errors: errors),
-          SizedBox(height: getProportionateScreenHeight(20)),
-          DefaultButton(
-            text: 'Continue',
-            press: () {
-              if (_formKey.currentState.validate()) {
-                _formKey.currentState.save();
-              }
-            },
-          )
-        ],
-      ),
-    );
-  }
-
-  TextFormField buildPasswordFormField() {
-    return TextFormField(
-      // obscure visibility of the password
-      obscureText: true,
-      onSaved: (newValue) => password = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kPassNullError)) {
-          setState(() {
-            errors.remove(kPassNullError);
-          });
-        } else if (value.length >= 8 && errors.contains(kShortPassError)) {
-          setState(() {
-            errors.remove(kShortPassError);
-          });
-        }
-        // In case a user removed some characters below the threshold, show alert
-        else if (value.length < 8 &&
-            value.isNotEmpty &&
-            !errors.contains(kShortPassError)) {
-          setState(() {
-            errors.add(kShortPassError);
-          });
-        }
-        return null;
-      },
-      validator: (value) {
-        if (value.isEmpty && !errors.contains(kPassNullError)) {
-          setState(() {
-            errors.add(kPassNullError);
-          });
-        } else if (value.length < 8 &&
-            value.isNotEmpty &&
-            !errors.contains(kShortPassError)) {
-          setState(() {
-            errors.add(kShortPassError);
-          });
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        // uses the InputDecorationTheme defined in my theme.dart file
-        labelText: "Password",
-        hintText: "Enter your password",
-        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          svgIcon: "assets/icons/Lock.svg",
-        ),
-      ),
-    );
-  }
-
-  TextFormField buildEmailFormField() {
-    return TextFormField(
-      // Requests a keyboard with ready access to the "@" and "." keys.
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (newValue) => email = newValue,
-      onChanged: (value) {
-        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.remove(kEmailNullError);
-          });
-        } else if (emailValidatorRegExp.hasMatch(value) &&
-            errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.remove(kInvalidEmailError);
-          });
-        }
-        return null;
-      },
-      validator: (value) {
-        if (value.isEmpty && !errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.add(kEmailNullError);
-          });
-        } else if (value.isNotEmpty &&
-            !emailValidatorRegExp.hasMatch(value) &&
-            !errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.add(kInvalidEmailError);
-          });
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        // uses the InputDecorationTheme defined in my theme.dart file
-        labelText: "Email",
-        hintText: "Enter your email",
-        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          svgIcon: "assets/icons/Mail.svg",
-        ),
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
+        padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+        height: getProportionateScreenHeight(40),
+        width: getProportionateScreenWidth(40),
+        decoration:
+            BoxDecoration(color: Color(0xFFF5F6F9), shape: BoxShape.circle),
+        child: SvgPicture.asset(icon),
       ),
     );
   }
---
+++ online_store/lib/screens/sign_in/components/sign_form.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:online_store/components/custom_suffix_icon.dart';
+import 'package:online_store/components/default_button.dart';
+import 'package:online_store/components/form_error.dart';
+import 'package:online_store/constants.dart';
+import 'package:online_store/size_config.dart';
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  // GlobalKey This uniquely identifies the Form , and allows validation of the form in a later step.
+  final _formKey = GlobalKey<FormState>();
+  String email;
+  String password;
+  bool remember = false;
+  final List<String> errors = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          // TextFormField - Creates a [FormField] that contains a [TextField].
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value;
+                  });
+                },
+              ),
+              Text("Remember me"),
+              Spacer(),
+              Text(
+                "Forgot Password?",
+                style: TextStyle(decoration: TextDecoration.underline),
+              )
+            ],
+          ),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(20)),
+          DefaultButton(
+            text: 'Continue',
+            press: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      // obscure visibility of the password
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kPassNullError)) {
+          setState(() {
+            errors.remove(kPassNullError);
+          });
+        } else if (value.length >= 8 && errors.contains(kShortPassError)) {
+          setState(() {
+            errors.remove(kShortPassError);
+          });
+        }
+        // In case a user removed some characters below the threshold, show alert
+        else if (value.length < 8 &&
+            value.isNotEmpty &&
+            !errors.contains(kShortPassError)) {
+          setState(() {
+            errors.add(kShortPassError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kPassNullError)) {
+          setState(() {
+            errors.add(kPassNullError);
+          });
+        } else if (value.length < 8 &&
+            value.isNotEmpty &&
+            !errors.contains(kShortPassError)) {
+          setState(() {
+            errors.add(kShortPassError);
+          });
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        // uses the InputDecorationTheme defined in my theme.dart file
+        labelText: "Password",
+        hintText: "Enter your password",
+        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(
+          svgIcon: "assets/icons/Lock.svg",
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      // Requests a keyboard with ready access to the "@" and "." keys.
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.remove(kEmailNullError);
+          });
+        } else if (emailValidatorRegExp.hasMatch(value) &&
+            errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.remove(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.add(kEmailNullError);
+          });
+        } else if (value.isNotEmpty &&
+            !emailValidatorRegExp.hasMatch(value) &&
+            !errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.add(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        // uses the InputDecorationTheme defined in my theme.dart file
+        labelText: "Email",
+        hintText: "Enter your email",
+        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(
+          svgIcon: "assets/icons/Mail.svg",
+        ),
+      ),
+    );
+  }
+}
