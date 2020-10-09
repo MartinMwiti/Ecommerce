eb71f02170c831323258ffea6d9279b4ac6ba8c2
MartinMwiti <martinmwiti007@gmail.com>
2020-10-09 19:07:46 +0300

    update - singleChildScrollView

---
+++ online_store/lib/components/social_card.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_store/size_config.dart';
+
+class SocialCard extends StatelessWidget {
+  const SocialCard({
+    Key key,
+    this.icon,
+    this.press,
+  }) : super(key: key);
+
+  final String icon;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
+        padding: EdgeInsets.all(getProportionateScreenWidth(10)),
+        height: getProportionateScreenHeight(45),
+        width: getProportionateScreenWidth(45),
+        decoration:
+            BoxDecoration(color: Color(0xFFF5F6F9), shape: BoxShape.circle),
+        child: SvgPicture.asset(icon),
+      ),
+    );
+  }
+}
--- online_store/lib/screens/sign_in/components/body.dart
+++ online_store/lib/screens/sign_in/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_store/components/social_card.dart';
+import 'package:online_store/constants.dart';
 import 'package:online_store/screens/sign_in/components/sign_form.dart';
 import 'package:online_store/size_config.dart';
 
@@ -12,70 +13,64 @@ class Body extends StatelessWidget {
         child: Padding(
           padding:
               EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: ListView(
-            // Originally was Column, replaced with ListView to avoid overflowed pixel problem with keybard.
-            children: [
-              Text(
-                "Welcome Back",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Sign In with your email and password \n or continue with social media.",
-                textAlign: TextAlign.center,
-              ),
-              SignForm(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SocialCard(
-                    icon: "assets/icons/google-icon.svg",
-                    press: () {},
-                  ),
-                  SocialCard(
-                    icon: "assets/icons/facebook-2.svg",
-                    press: () {},
-                  ),                
-                  SocialCard(
-                    icon: "assets/icons/twitter.svg",
-                    press: () {},
-                  )
-                ],
-              )
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.01),
+                Text(
+                  "Welcome Back",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Sign In with your email and password \n or continue with social media.",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.06),
+                SignForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.03),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocialCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    )
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style:
+                          TextStyle(fontSize: getProportionateScreenWidth(16)),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          fontSize: getProportionateScreenWidth(16),
+                          color: kPrimaryColor),
+                    )
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SocialCard extends StatelessWidget {
-  const SocialCard({
-    Key key,
-    this.icon,
-    this.press,
-  }) : super(key: key);
-
-  final String icon;
-  final Function press;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin:
-            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(10)),
-        padding: EdgeInsets.all(getProportionateScreenWidth(12)),
-        height: getProportionateScreenHeight(40),
-        width: getProportionateScreenWidth(40),
-        decoration:
-            BoxDecoration(color: Color(0xFFF5F6F9), shape: BoxShape.circle),
-        child: SvgPicture.asset(icon),
       ),
     );
   }
