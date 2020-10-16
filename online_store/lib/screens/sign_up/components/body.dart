b90b9d4582997f24afe4d69e7f7c3499720b845d
MartinMwiti <martinmwiti007@gmail.com>
2020-10-16 08:57:22 +0300

    register form

--- online_store/lib/screens/sign_up/components/body.dart
+++ online_store/lib/screens/sign_up/components/body.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:online_store/components/custom_suffix_icon.dart';
+import 'package:online_store/components/default_button.dart';
 import 'package:online_store/constants.dart';
+import 'package:online_store/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          Text(
-            "Register Account",
-            style: headingStyle,
-          ),
-          Text(
-            "Complete your details or continue \nwith social media.",
-            textAlign: TextAlign.center,
-          ),
-          SignUpForm()
-        ],
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: Column(
+          children: [
+            Text(
+              "Register Account",
+              style: headingStyle,
+            ),
+            Text(
+              "Complete your details or continue \nwith social media.",
+              textAlign: TextAlign.center,
+            ),
+            SignUpForm()
+          ],
+        ),
       ),
     );
   }
@@ -36,7 +42,7 @@ class _SignUpFormState extends State<SignUpForm> {
   bool remember = false;
   final List<String> errors = [];
 
-  // func with named parameter for adding error 
+  // func with named parameter for adding error
   void addError({String error}) {
     if (!errors.contains(error))
       setState(() {
@@ -44,7 +50,7 @@ class _SignUpFormState extends State<SignUpForm> {
       });
   }
 
-  // func with named parameter for removing error 
+  // func with named parameter for removing error
   void removeError({String error}) {
     if (errors.contains(error))
       setState(() {
@@ -58,81 +64,134 @@ class _SignUpFormState extends State<SignUpForm> {
       key: _formKey,
       child: Column(
         children: [
-          TextFormField(
-            // Requests a keyboard with ready access to the "@" and "." keys.
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (newValue) => email = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-                removeError(error: kEmailNullError);
-              } else if (emailValidatorRegExp.hasMatch(value)) {
-                removeError(error: kInvalidEmailError);
-              } else if (value.isNotEmpty &&
-                  !emailValidatorRegExp.hasMatch(value)) {
-                addError(error: kInvalidEmailError);
-                return null;
-              }
-            },
-            validator: (value) {
-              if (value.isEmpty) {
-                addError(error: kEmailNullError);
-                removeError(error: kInvalidEmailError);
-              } else if (value.isNotEmpty &&
-                  !emailValidatorRegExp.hasMatch(value)) {
-                addError(error: kInvalidEmailError);
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildConfirmPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(40)),
+          DefaultButton(
+            text: "Register",
+            press: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
               }
               return null;
             },
-            decoration: InputDecoration(
-              // uses the InputDecorationTheme defined in my theme.dart file
-              labelText: "Email",
-              hintText: "Enter your email",
-              // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CustomSuffixIcon(
-                svgIcon: "assets/icons/Mail.svg",
-              ),
-            ),
-          ),
-          TextFormField(
-            // obscure visibility of the password
-            obscureText: true,
-            onSaved: (newValue) => password = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty && errors.contains(kPassNullError)) {
-                removeError(error: kPassNullError);
-              } else if (value.length >= 8) {
-                removeError(error: kShortPassError);
-              }
-              // In case a user removed some characters below the threshold, show alert
-              else if (value.length < 8 && value.isNotEmpty) {
-                addError(error: kShortPassError);
-              }
-              return null;
-            },
-            validator: (value) {
-              if (value.isEmpty) {
-                addError(error: kPassNullError);
-                removeError(error: kShortPassError);
-                return "";
-              } else if (value.length < 8 && value.isNotEmpty) {
-                addError(error: kShortPassError);
-                return "";
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              // uses the InputDecorationTheme defined in my theme.dart file
-              labelText: "Password",
-              hintText: "Enter your password",
-              // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: CustomSuffixIcon(
-                svgIcon: "assets/icons/Lock.svg",
-              ),
-            ),
-          ),
+          )
         ],
+      ),
+    );
+  }
+
+  TextFormField buildConfirmPasswordFormField() {
+    return TextFormField(
+      // obscure visibility of the password
+      obscureText: true,
+      onSaved: (newValue) => confirmPassword = newValue,
+      onChanged: (value) {
+        if (confirmPassword == password && errors.contains(kMatchPassError)) {
+          removeError(error: kMatchPassError);
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          return "";
+        } else if (confirmPassword != password) {
+          addError(error: kMatchPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        // uses the InputDecorationTheme defined in my theme.dart file
+        labelText: "Confirm Password",
+        hintText: "Re-enter your password",
+        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(
+          svgIcon: "assets/icons/Lock.svg",
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      // obscure visibility of the password
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kPassNullError)) {
+          removeError(error: kPassNullError);
+        } else if (value.length >= 8) {
+          removeError(error: kShortPassError);
+        }
+        // In case a user removed some characters below the threshold, show alert
+        else if (value.length < 8 && value.isNotEmpty) {
+          addError(error: kShortPassError);
+        }
+        password = value;
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kPassNullError);
+          removeError(error: kShortPassError);
+          return "";
+        } else if (value.length < 8 && value.isNotEmpty) {
+          addError(error: kShortPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        // uses the InputDecorationTheme defined in my theme.dart file
+        labelText: "Password",
+        hintText: "Enter your password",
+        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(
+          svgIcon: "assets/icons/Lock.svg",
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      // Requests a keyboard with ready access to the "@" and "." keys.
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+          removeError(error: kEmailNullError);
+        } else if (emailValidatorRegExp.hasMatch(value)) {
+          removeError(error: kInvalidEmailError);
+        } else if (value.isNotEmpty && !emailValidatorRegExp.hasMatch(value)) {
+          addError(error: kInvalidEmailError);
+          return null;
+        }
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: kEmailNullError);
+          removeError(error: kInvalidEmailError);
+        } else if (value.isNotEmpty && !emailValidatorRegExp.hasMatch(value)) {
+          addError(error: kInvalidEmailError);
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        // uses the InputDecorationTheme defined in my theme.dart file
+        labelText: "Email",
+        hintText: "Enter your email",
+        // When [FloatingLabelBehavior.always] the label will always float at the top of the field above the content.
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSuffixIcon(
+          svgIcon: "assets/icons/Mail.svg",
+        ),
       ),
     );
   }
