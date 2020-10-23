83efd1c6a5b482bdf9a55171aa2f68b8323f84ec
MartinMwiti <martinmwiti007@gmail.com>
2020-10-23 11:33:53 +0300

    home page update

--- online_store/lib/screens/home/components/body.dart
+++ online_store/lib/screens/home/components/body.dart
@@ -42,49 +42,81 @@ class Body extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Stack(
-                    overflow: Overflow
-                        .visible, // Whether overflowing children shouldn't be clipped/cut. In our case the 2nd container which has -3 top is clipped.
-                    children: [
-                      Container(
-                        padding:
-                            EdgeInsets.all(getProportionateScreenWidth(12)),
-                        height: getProportionateScreenWidth(46),
-                        width: getProportionateScreenWidth(46),
-                        decoration: BoxDecoration(
-                            color: kSecondaryColor.withOpacity(0.1),
-                            shape: BoxShape.circle),
-                        child: SvgPicture.asset("assets/icons/Bell.svg"),
-                      ),
-                      Positioned(
-                        top: -3,
-                        right: 0,
-                        child: Container(
-                          height: getProportionateScreenWidth(16),
-                          width: getProportionateScreenWidth(16),
-                          decoration: BoxDecoration(
-                            color: Color(0xFFFF48484),
-                            shape: BoxShape.circle,
-                            border: Border.all(width: 1.5, color: Colors.white),
-                          ),
-                          child: Center(
-                            child: Text("3",
-                                style: TextStyle(
-                                  fontSize: getProportionateScreenWidth(10),
-                                  height: 1, // The height of this text span, as a multiple of the font size. fontSize * height
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                          ),
-                        ),
-                      )
-                    ],
-                  )
+                  IconBtnWithCounter(
+                    svgSrc: "assets/icons/Cart Icon.svg",
+                    press: (){},
+                  ),
+                  IconBtnWithCounter(
+                    svgSrc: "assets/icons/Bell.svg",
+                    numOfItems: 3,
+                    press: (){},
+                  ),
                 ],
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class IconBtnWithCounter extends StatelessWidget {
+  const IconBtnWithCounter({
+    Key key,
+    @required this.svgSrc,
+    this.numOfItems = 0,
+    @required this.press,
+  }) : super(key: key);
+
+  final String svgSrc;
+  final int numOfItems;
+  final GestureTapCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      // A rectangular area of a Material that responds to touch.
+      onTap: press,
+      borderRadius: BorderRadius.circular(50),
+      child: Stack(
+        overflow: Overflow
+            .visible, // Whether overflowing children shouldn't be clipped/cut. In our case the 2nd container which has -3 top is clipped.
+        children: [
+          Container(
+            padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+            height: getProportionateScreenWidth(46),
+            width: getProportionateScreenWidth(46),
+            decoration: BoxDecoration(
+                color: kSecondaryColor.withOpacity(0.1),
+                shape: BoxShape.circle),
+            child: SvgPicture.asset(svgSrc),
+          ),
+          if (numOfItems !=0) // if numOfItems is not equal to 0, display the 2nd stack widget(the positioned Container)
+          Positioned(
+            top: -3,
+            right: 0,
+            child: Container(
+              height: getProportionateScreenWidth(16),
+              width: getProportionateScreenWidth(16),
+              decoration: BoxDecoration(
+                color: Color(0xFFFF48484),
+                shape: BoxShape.circle,
+                border: Border.all(width: 1.5, color: Colors.white),
+              ),
+              child: Center(
+                child: Text("$numOfItems",
+                    style: TextStyle(
+                      fontSize: getProportionateScreenWidth(10),
+                      height:
+                          1, // The height of this text span, as a multiple of the font size. fontSize * height
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    )),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
