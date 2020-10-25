a9ac429000d5611e58c60d84f76d57d6f8cd8853
MartinMwiti <martinmwiti007@gmail.com>
2020-10-25 16:17:16 +0300

    models

--- online_store/lib/screens/home/components/body.dart
+++ online_store/lib/screens/home/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:online_store/screens/home/components/categories.dart';
 import 'package:online_store/screens/home/components/discount_banner.dart';
 import 'package:online_store/screens/home/components/home_header.dart';
+import 'package:online_store/screens/home/components/section_title.dart';
 import 'package:online_store/screens/home/components/special_offers.dart';
 import 'package:online_store/size_config.dart';
 
@@ -14,12 +15,17 @@ class Body extends StatelessWidget {
           children: [
             SizedBox(height: getProportionateScreenWidth(20)),
             HomeHeader(),
-            SizedBox(height: getProportionateScreenWidth(30)),
+            SizedBox(height: getProportionateScreenWidth(25)),
             DiscountBanner(),
-            SizedBox(height: getProportionateScreenWidth(30)),
+            SizedBox(height: getProportionateScreenWidth(25)),
             Categories(),
-            SizedBox(height: getProportionateScreenWidth(30)),
+            SizedBox(height: getProportionateScreenWidth(20)),
             SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(20)),
+            SectionTitle(
+              text: "Popular Products",
+              press: () {},
+            )
           ],
         ),
       ),
